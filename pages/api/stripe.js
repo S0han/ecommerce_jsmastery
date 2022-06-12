459254a47e142d3fb77db4cfc61ba82f4a38276d
S0han <43890209+S0han@users.noreply.github.com>
2022-06-12 06:12:00 -0400

    final round

---
+++ pages/api/stripe.js
@@ -0,0 +1,49 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        try {
+            const params = {
+                submit_type: 'pay',
+                mode: 'payment',
+                payment_method_types: ['card'],
+                billing_address_collection: 'auto',
+                shipping_options: [
+                    { shipping_rate: 'shr_1L9mINGSHiYlc8vhKtJo4Iqd'},
+                    { shipping_rate: 'shr_1L9mK8GSHiYlc8vhApzz9K80'},
+                ],
+                line_items: req.body.cartItems.map((item) => {
+                    const img = item.image[0].asset._ref;
+                    const newImage = img.replace('image-', 'https://cdn.sanity.io/images/puczbzxz/production').replace('-webp', '.webp');
+
+                    return {
+                        price_data: {
+                            currency: 'cad',
+                            product_data: {
+                                name: item.name,
+                                images: [newImage],
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        adjustable_quantity: {
+                            enabled: true,
+                            minimum: 1,
+                        },
+                        quantity: item.quantity
+                    }
+                }),
+                success_url: `${req.headers.origin}/success`,
+                cancel_url: `${req.headers.origin}/canceled`,
+            };
+            // Create Checkout Sessions from body params.
+            const session = await stripe.checkout.sessions.create(params);
+            
+            res.redirect(200).json(session);
+        } catch (error) {
+            res.status(500).json({ statusCode: 500, message: error.message });
+        }
+    }
+}
+
